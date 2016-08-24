78456f76273f149e564741160810ab292a4cfffb
localadmin <localadmin@Hatchs-MacBook-Pro.local>
2016-08-24 19:24:46 -0400

    v1.0.0 update

--- build/js/main.js
+++ build/js/main.js
@@ -1,2 +1,18 @@
-var bp = require('./bluePainter.js');
-bp.paint();
+function getPageSize() {
+	return {
+		height: Math.max(document.documentElement.clientHeight, window.innerHeight || 0),
+		width: Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
+	};
+}
+
+function setHeight(){
+	var pages = document.getElementsByClassName('page');
+	pageSize = getPageSize();
+	for (var i = 0; i < pages.length; i++) {
+		pages[i].style.width = pageSize.width + 'px';
+		pages[i].style.height = (pageSize.height - 75) + 'px';
+	}
+}
+
+// window.onresize = setHeight;
+// document.addEventListener("DOMContentLoaded", setHeight);
